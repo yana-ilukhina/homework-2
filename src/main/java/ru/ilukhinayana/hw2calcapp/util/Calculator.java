9d8d5b37fa51171dcc9c05d177f538d8bd622d19
yana-ilukhina <ilukhina.yana@gmail.com>
2024-10-08 15:21:21 +0300

    add service class UserGreeting with method greeting. minor change

--- src/main/java/ru/ilukhinayana/hw2calcapp/util/Calculator.java
+++ src/main/java/ru/ilukhinayana/hw2calcapp/util/Calculator.java
@@ -2,17 +2,15 @@ package ru.ilukhinayana.hw2calcapp.util;
 
 public class Calculator {
 
-    public static double sum (double num1, double num2){ // метод Сложения
-        double summi = num1 + num2;
-        return summi;
+    public static double sum(double num1, double num2) { // метод Сложения
+        return num1 + num2;
     }
 
-    public static double subtract (double num1, double num2){ // метод Вычитания
+    public static double subtract(double num1, double num2) { // метод Вычитания
         return num1 - num2;
     }
 
-    public static double multiply (double num1, double num2){ // метод Умножения
+    public static double multiply(double num1, double num2) { // метод Умножения
         return num1 * num2;
     }
-
 }
