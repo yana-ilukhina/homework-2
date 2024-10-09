41af3870a801c322a1dcc5ea915840de67aeda5d
yana-ilukhina <ilukhina.yana@gmail.com>
2024-10-09 23:23:37 +0300

    add comment, minor changes

--- src/main/java/ru/ilukhinayana/hw2calcapp/service/ResultWriterService.java
+++ src/main/java/ru/ilukhinayana/hw2calcapp/service/ResultWriterService.java
@@ -1,6 +1,7 @@
 package ru.ilukhinayana.hw2calcapp.service;
 
 public class ResultWriterService {
+
     public static void resultWriter(double num1, double num2, double result, String operation) { // метод Вывода результата
         System.out.println("Операция " + operation + " с числами " + num1 + " и " + num2 +
                 ", результат " + result);
